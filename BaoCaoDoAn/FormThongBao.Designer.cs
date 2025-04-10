cb501474ccad6adb8645fcab7f9555822270ec56
TMQ3105 <Quanminhtran3105@gmail.com>
2025-04-10 22:18:37 +0700

    Tạo Form Thông báo_US3.9

--- BaoCaoDoAn/FormThongBao.Designer.cs
+++ BaoCaoDoAn/FormThongBao.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,19 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Thông báo đơn hàng";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(323, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
             // FormThongBao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "FormThongBao";
             this.Text = "FormThonBao";
@@ -56,5 +65,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
